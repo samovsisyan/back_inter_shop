1b1842269b881fbc5000d05f2c26fe20c63a88a8
samovsisyan <samovsisyan9@gmail.com>
2020-05-16 06:24:59 +0400

    Setings routes/regions

---
+++ routes/regions.js
@@ -0,0 +1,22 @@
+const models = require('../models');
+const express = require('express');
+
+const router = express.Router();
+
+
+router.get('/', async (req, res, next) => {
+    try {
+
+
+        const regions = await models.Regions.findAll();
+        res.json({
+            status: 'ok',
+            regions,
+        })
+    } catch (e) {
+        next(e)
+
+    }
+});
+
+module.exports = router;
